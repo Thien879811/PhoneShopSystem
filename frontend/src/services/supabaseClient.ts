a856160c0a4599e3a4c6aaf2ab2643f23600141d
Thien <thienlhg0318@gmail.com>
2026-03-30 13:42:03 +0700

    chore: supabase

--- frontend/src/services/supabaseClient.ts
+++ frontend/src/services/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://meaizdrxqalzvgtppxdi.supabase.co';
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_hGygmoGqzrgbHo9SOjul4A_v-Coj1XD';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
